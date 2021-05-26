b749dc873d1e2e2edb199ff71b1e98c12afa67aa
nelo <nelokwaz@gmail.com>
2021-05-26 14:03:45 +0100

    created a routes folder to hold the routes of the backend application

--- backend/server.js
+++ backend/server.js
@@ -3,10 +3,23 @@ const morgan = require("morgan");
 const bodyParser = require("body-parser");
 const cookieParser = require("cookie-parser");
 const cors = require("cors");
+const mongoose = require("mongoose");
 require("dotenv").config();
 
 //app
 const app = express();
+
+//db
+mongoose
+  .connect(process.env.DATABASE_LOCAL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  })
+  .then(() => {
+    console.log("db connection established");
+  });
 
 //middlewares
 app.use(morgan("dev"));
