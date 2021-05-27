d3456e044cdd8fbcdfd12b0809e0662534ac5527
nelo <nelokwaz@gmail.com>
2021-05-27 20:14:55 +0100

    added timestamps to the user model

--- backend/models/user.js
+++ backend/models/user.js
@@ -1,51 +1,54 @@
 const mongoose = require("mongoose");
 const crypto = require("crypto");
 
-const userSchema = new mongoose.Schema({
-  username: {
-    type: String,
-    trim: true,
-    required: true,
-    maxLength: 32,
-    unique: true,
-    index: true, //indexable,
-    lowerCase: true,
+const userSchema = new mongoose.Schema(
+  {
+    username: {
+      type: String,
+      trim: true,
+      required: true,
+      maxLength: 32,
+      unique: true,
+      index: true, //indexable,
+      lowerCase: true,
+    },
+    name: {
+      type: String,
+      trim: true,
+      required: true,
+      maxLength: 32,
+    },
+    email: {
+      type: String,
+      trim: true,
+      required: true,
+      unique: email,
+      lowerCase: true,
+    },
+    profile: {
+      type: String,
+      required: true,
+    },
+    hashed_password: {
+      type: String,
+      required: true,
+    },
+    salt: String,
+    about: {
+      type: String,
+    },
+    role: {
+      type: number,
+      trim: true,
+    },
+    photo: {
+      data: Buffer, //mongodb is good for saving binary data
+      contentType: String,
+    },
+    resetPasswordLink: {
+      data: String,
+      default: "",
+    },
   },
-  name: {
-    type: String,
-    trim: true,
-    required: true,
-    maxLength: 32,
-  },
-  email: {
-    type: String,
-    trim: true,
-    required: true,
-    unique: email,
-    lowerCase: true,
-  },
-  profile: {
-    type: String,
-    required: true,
-  },
-  hashed_password: {
-    type: String,
-    required: true,
-  },
-  salt: String,
-  about: {
-    type: String,
-  },
-  role: {
-    type: number,
-    trim: true,
-  },
-  photo: {
-    data: Buffer, //mongodb is good for saving binary data
-    contentType: String,
-  },
-  resetPasswordLink: {
-    data: String,
-    default: "",
-  },
-});
+  { timestamp: true } // the timestamp automatically adds created and updated date time stamps to the database
+);
