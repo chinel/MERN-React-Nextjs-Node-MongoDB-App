57bc3c2e4620c9e13a08abd8130f3c0c9b8d6e4b
nelo <nelokwaz@gmail.com>
2021-05-28 20:23:06 +0100

    added the auth route as a middleware

--- backend/server.js
+++ backend/server.js
@@ -7,6 +7,7 @@ const mongoose = require("mongoose");
 require("dotenv").config();
 //Import routes
 const blogRoutes = require("./routes/blog");
+const authRoutes = require("./routes/auth");
 
 //app
 const app = express();
@@ -35,6 +36,7 @@ if (process.env.NODE_ENV === "development") {
 
 //routes middleware
 app.use("/api", blogRoutes);
+app.use("/api", authRoutes);
 
 //port
 const port = process.env.PORT || 8000;
