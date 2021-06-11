5d07d958e3cfd21b853a18fe611542d76b174de4
nelo <nelokwaz@gmail.com>
2021-06-11 15:06:13 +0100

    added a test route to test authenticated user's access

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,6 +1,11 @@
 const express = require("express");
 const router = express.Router();
-const { signup, signin, signout } = require("../controllers/auth");
+const {
+  signup,
+  signin,
+  signout,
+  requireSignin,
+} = require("../controllers/auth");
 
 // validators
 const { runValidation } = require("../validators");
@@ -13,5 +18,12 @@ const {
 router.post("/signup", userSignupValidator, runValidation, signup);
 router.post("/signin", userSigninValidator, runValidation, signin);
 router.get("/signout", signout);
+
+//testing
+router.get("/secret", requireSignin, (req, res) => {
+  res.json({
+    message: "you have access to secret page",
+  });
+});
 
 module.exports = router;
