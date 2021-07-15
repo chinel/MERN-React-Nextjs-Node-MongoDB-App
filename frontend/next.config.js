d4b48ab24d07ecb04ce12213d870cb6305af1ca3
nelo <nelokwaz@gmail.com>
2021-07-15 05:27:37 +0100

    added some environmental variables

---
+++ frontend/next.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  publicRuntimeConfig: {
+    //will be avialable on both server and client
+    APP_NAME: "SEO BLOG",
+    API_DEVELOPMENT: "http://localhost:8000/api",
+    PRODUCTION: false,
+  },
+};
