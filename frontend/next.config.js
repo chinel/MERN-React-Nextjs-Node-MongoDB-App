446885c85b35e6402e57aaf18a61735000613aaa
nelo <nelokwaz@gmail.com>
2021-07-15 05:39:32 +0100

    used the environment variable in the Header component

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   publicRuntimeConfig: {
     //will be avialable on both server and client
-    APP_NAME: "SEO BLOG",
+    APP_NAME: "SEOBLOG",
     API_DEVELOPMENT: "http://localhost:8000/api",
     PRODUCTION: false,
   },
