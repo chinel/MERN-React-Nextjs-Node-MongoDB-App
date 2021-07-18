a3cb84414f2a0bc16892cdc90797d6e2af9cbbd7
nelo <nelokwaz@gmail.com>
2021-07-18 06:47:53 +0100

    added form element and a function to handle on submit and on change

--- frontend/components/auth/SignupComponent.js
+++ frontend/components/auth/SignupComponent.js
@@ -1,5 +1,28 @@
 const SignupComponent = () => {
-  return <h2>Signup</h2>;
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log("handle submit");
+  };
+
+  const handleChange = (e) => {
+    console.log(e.target.value);
+  };
+
+  const signupForm = () => {
+    return (
+      <form onSubmit={handleSubmit}>
+        <div className="form-group">
+          <input
+            onChange={handleChange}
+            type="text"
+            className="form-control"
+            placeholder="Type your name"
+          />
+        </div>
+      </form>
+    );
+  };
+  return <>{signupForm()}</>;
 };
 
 export default SignupComponent;
