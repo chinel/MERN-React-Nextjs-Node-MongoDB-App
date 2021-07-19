1e2d7225068e2379a60985b0d2aab828861e6bfc
nelo <nelokwaz@gmail.com>
2021-07-19 08:03:44 +0100

    added the production url to the configuration file

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -3,6 +3,7 @@ module.exports = {
     //will be avialable on both server and client
     APP_NAME: "SEOBLOG",
     API_DEVELOPMENT: "http://localhost:8000/api",
+    API_PRODUCTION: "https://seoblog.com",
     PRODUCTION: false,
   },
 };
