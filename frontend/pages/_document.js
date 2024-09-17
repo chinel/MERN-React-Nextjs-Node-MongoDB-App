43ebea9e321f96f4fc36bb685c468fa363153d1f
nelo <nelokwaz@gmail.com>
2024-09-17 08:08:30 +0100

    fix: fix meta attribute

--- frontend/pages/_document.js
+++ frontend/pages/_document.js
@@ -4,7 +4,7 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
-        <meta charset="UTF-8" />
+        <meta charSet="UTF-8" />
         <meta view="viewport" content="width=device-width, initial-scale=1.0" />
       </Head>
       <body>
